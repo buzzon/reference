1cabd5982ae422d63e9e0270ebf98a90bf6706d6
buzzon <buzzondev@gmail.com>
2020-11-27 06:31:46 +0500

    Board Admin

--- reference/core/admin.py
+++ reference/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'created')
+
+
+admin.site.register(Board, BoardAdmin)
